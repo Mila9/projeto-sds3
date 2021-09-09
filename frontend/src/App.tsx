ed7a34717b20c7d1a0a3017f0c332c303576d790
Mila9 <barbosa196milena@gmail.com>
2021-09-09 14:09:53 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import DataTable from "components/DataTable";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
+    <>
+    <div className="container">
+    <NavBar/>
       <h1 className= "text-primary">Olá Mundo</h1>
+      <DataTable/>
     </div>
+    </>
   );
 }
 
